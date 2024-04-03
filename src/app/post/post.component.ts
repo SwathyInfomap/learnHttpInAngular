1ae21aa030f68a019ab062bfe6b75f7df37a23fa
SwathyInfomap <Swathy@infomapglobal.com>
2024-04-03 15:03:35 +0530

    generated services to implement API calls

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { PostService } from '../services/post.service';
 
 @Component({
   selector: 'app-post',
@@ -9,17 +10,25 @@ import { Component, inject } from '@angular/core';
   styleUrl: './post.component.css'
 })
 export class PostComponent {
-  http= inject(HttpClient);
+  //http= inject(HttpClient);
+  private postService = inject(PostService)
   posts:any=[];
 
   ngOnInit(): void{
-      this.fetchPosts();
+      //this.fetchPosts();
+      this.loadPosts();
   }
-  fetchPosts(){
-    /**to retrieve posts from the API */
-    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe((posts:any)=>{
+  // fetchPosts(){
+  //   /**to retrieve posts from the API */
+  //   this.http.get('https://jsonplaceholder.typicode.com/posts?_limit=10').subscribe((posts:any)=>{
+  //     console.log(posts);
+  //     this.posts = posts;
+  //   });
+  // }
+  loadPosts(){
+    this.postService.getPosts().subscribe((posts:any)=>{
       console.log(posts);
       this.posts = posts;
-    });
+    })
   }
 }
